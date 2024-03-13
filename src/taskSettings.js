50d316ef34075d50923f2d744e4978acd6d31110
Kevin <epickillerdude2007@gmail.com>
2024-03-12 20:16:22 -0400

    fixing files

--- src/taskSettings.js
+++ src/taskSettings.js
@@ -1,7 +1,5 @@
 import addTask from "./makeTask";
 
 for(let i = 0; i < document.querySelectorAll('.projectBox')) {
-    for(let i = 0; i < document.querySelectorAll('.taskbox')) {
-        element = 0
-    }
+    
 }

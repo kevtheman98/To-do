58f4bfb39b6b07519a4226843f6a3d66dd9c9461
Kevin <epickillerdude2007@gmail.com>
2024-03-12 21:08:58 -0400

    added delete functionality

--- src/makeTask.js
+++ src/makeTask.js
@@ -1,15 +1,31 @@
-export default function addTask(currentProject) {
+export default function makeTask(currentProject) {
+   
    if (currentProject) {
       const taskBox = document.createElement("div")
       taskBox.classList = "taskBox"
       const todo = document.createElement("input")
-      const delbutton = document.createElement('button')
-      delbutton.textContent = "Delete"
-      currentProject.appendChild(taskBox)
       taskBox.appendChild(todo)
-      taskBox.appendChild(delbutton)
+      const settingfunc = settings()
+      settingfunc.delbutton()
+      function settings() {
+         function delbutton() {
+            const delbutton = document.createElement('button')
+            delbutton.textContent = "Delete"
+            currentProject.appendChild(taskBox)
+            taskBox.appendChild(delbutton)
+
+            delbutton.addEventListener('click', () => {
+               taskBox.remove()
+            })
+         }
+
+
+         return { delbutton }
+      }
+      
    } else {
       console.log("This is teh fisrt one")
    }
 
 }
+

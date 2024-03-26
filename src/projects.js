48a29181c6428f71b05e2d686fef663ac763baa4
Kevin Kodiyan <kodiyankevin@gmail.com>
2024-03-25 21:31:09 -0400

    added count to project class

--- src/projects.js
+++ src/projects.js
@@ -1,15 +1,19 @@
 const projectsTab = document.querySelector(".projects")
-
+let count = 1
 export function createProject(title, dueDate, description) {
+    const projectCountainer = document.createElement('div')
+    projectCountainer.classList = count + "projectCountainer"
+    count += 1
     const titleBox = document.createElement('div')
     const dueDateBox = document.createElement('div')
     const descriptionBox = document.createElement('div')
     titleBox.textContent = title
     dueDateBox.textContent = dueDate
     descriptionBox.textContent = description
-    projectsTab.appendChild(titleBox)
-    projectsTab.appendChild(dueDateBox)
-    projectsTab.appendChild(descriptionBox)
+    projectCountainer.appendChild(titleBox)
+    projectCountainer.appendChild(dueDateBox)
+    projectCountainer.appendChild(descriptionBox)
+    projectsTab.appendChild(projectCountainer)
     
 
 }

86c2f7ce21123b0e90fb173ada71b2eb380847b9
Kevin Kodiyan <kodiyankevin@gmail.com>
2024-03-25 21:05:44 -0400

    dom for create projects

--- src/projects.js
+++ src/projects.js
@@ -1,7 +1,17 @@
-
+const projectsTab = document.querySelector(".projects")
 
 export function createProject(title, dueDate, description) {
-    console.log(title)
+    const titleBox = document.createElement('div')
+    const dueDateBox = document.createElement('div')
+    const descriptionBox = document.createElement('div')
+    titleBox.textContent = title
+    dueDateBox.textContent = dueDate
+    descriptionBox.textContent = description
+    projectsTab.appendChild(titleBox)
+    projectsTab.appendChild(dueDateBox)
+    projectsTab.appendChild(descriptionBox)
+    
+
 }
 
 
